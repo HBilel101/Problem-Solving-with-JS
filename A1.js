2c543dfb5d2149b6736300299255cff93c36d799
Bilel <90275355+HBilel101@users.noreply.github.com>
2022-06-15 14:35:23 +0100

    Bonus Section
    i've done Bonus Section

--- A1.js
+++ A1.js
@@ -46,3 +46,38 @@ for(let i=0;i<arr.length;i++){
 return result ;
 }
 console.log(solution1(array)) ;
+
+/* BONUS:
+In this section , I used the code of firattale and added some features so it works perfectly and gives you the expected result
+you can find his code https://github.com/firattale/JavaScript/blob/master/ArrayFunctions/Andrew's%20Questions/A1.js
+*/
+
+let arr = ["1", 2, "4", "591", 392, "391", 2, 5, "10", 2, "1", "1", 1, 20, 20];
+
+arrSort(arr);
+
+
+function arrSort(argument) {
+    const result = [] ;
+    switch (typeof arr[0]) {
+        case "string":
+            let arr2 = arr.filter(function(num, i) {
+                return typeof arr[i] == "string";
+            });
+            let arr3 = arr.filter(function(num, i) {
+                return typeof arr[i] == "number";
+            });
+            result.push(arr3.sort((a,b)=>a-b),arr2.sort((a,b)=>Number(a)-Number(b)));
+            break;
+        case "number":
+            let arr4 = arr.filter(function(num, i) {
+                return typeof arr[i] == "number";
+            });
+            let arr5 = arr.filter(function(num, i) {
+                return typeof arr[i] == "string";
+            });
+            result.push(arr4.sort((a,b)=>a-b),arr5.sort((a,b)=>Number(a)-Number(b)));
+            break;
+    }
+    return result;
+}
